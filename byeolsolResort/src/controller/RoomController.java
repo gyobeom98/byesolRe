aa22f81a13376e7e5c10ef088257baf8a9f99e4b
gyobeom <gyobeom29@gmail.com>
2020-07-07 10:46:50 +0900

    update 20/07/07 오전 10:46 response

--- byeolsolResort/src/controller/RoomController.java
+++ byeolsolResort/src/controller/RoomController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import model.dto.Room;
 import model.service.RoomService;
 
 @Controller
@@ -23,6 +26,15 @@ public class RoomController {
 		m.addAttribute("roomList",roomService.getRoomListByConcept(concept));
 		return "roomInfo";
 	}
+	
+	@PostMapping("/getPeopleCount")
+	@ResponseBody
+	public int sendMaxPeopleCount(int roomNum) {
+		Room room =roomService.getRoomByRoomNum(roomNum);
+		return room.getMaxPeople();
+		
+	}
+	
 	
 	
 //	@GetMapping("/updateRoom")

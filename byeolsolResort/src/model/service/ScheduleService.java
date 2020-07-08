5d771b43234cd382de5a93fc91ab292b7b2d54c9
gyobeom <gyobeom29@gmail.com>
2020-07-08 18:20:28 +0900

    update 20/07/08 오후 06:20  ScheduleService

--- byeolsolResort/src/model/service/ScheduleService.java
+++ byeolsolResort/src/model/service/ScheduleService.java
@@ -1,6 +1,7 @@
 package model.service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -24,8 +25,16 @@ public class ScheduleService {
 		List<Reserv> reservList = reservMapper.selectReservList();
 		if (reservList.size() > 0) {
 			LocalDate now = LocalDate.now();
+			LocalDateTime nowWithTime = LocalDateTime.now();
 			for (Reserv reserv : reservList) {
-				if (now.compareTo(reserv.getRegDate().toLocalDate()) >= 2) {
+				if(reserv.getState().equals("미입금")) {
+					if(reserv.getStartDate().equals(now) && nowWithTime.getHour()>11 || reserv.getStartDate().equals(now) && nowWithTime.getHour()==11 && nowWithTime.getMinute()>=30) {
+						reservMapper.deleteReserv(reserv.getId());
+					}
+					if(now.compareTo(reserv.getRegDate().toLocalDate())>= 2) {
+						reservMapper.deleteReserv(reserv.getId());
+					}
+				}else if(reserv.getEndDate().equals(now)) {
 					reservMapper.deleteReserv(reserv.getId());
 				}
 			}

0560fe475f401dfc36c551bfcd9fb3ea4e5b380f
gyobeom <gyobeom29@gmail.com>
2020-07-16 10:40:37 +0900

    update 20/07/16 오전 10:40

--- byeolsolResort/src/model/service/IndexService.java
+++ byeolsolResort/src/model/service/IndexService.java
@@ -11,23 +11,27 @@ public class IndexService {
 		
 		switch(classification) {
 		case "mainImg" : case "byeolsolinfo": case "roomMain": case "healingRoom": case "gameRoom": case "kidsRoom": 
-		case "trip": case "moun": case "golf": case "logo": 
+		case "trip": case "moun": case "golf": case "logo": case "question": case "reserv": 
 			path += "/index"; break;
 		case "board" : path+="/board"; break;
 		case "event" : path +="/event"; break;
-		case "reserv": path += "/reserv"; break;
-		case "service": path += "/cus"; break;
+		case "myPage": path += "/cus"; break;
 		}
 		
 		switch(value) {
-		case "main": case "logo" :  path+="/main"; break;
+		case "main": case "title1" : case "title2":  path+="/main"; break;
 		case "R1subImg": path += "/roomInfo_01"; break;
 		case "R2subImg": path += "/roomInfo_02"; break;
 		case "R3subImg": path += "/roomInfo_03"; break;
 		case "RMsubimg": path += "/roomMain"; break;
 		case "byeolsolnewssub" : path+="/list"; break;
-		case "" : path+="adminUserInfo"; break;
+		case "myPageSub": path+="adminUserInfo"; break;
+		case "moun2": path += "/moun"; break;
+		case "tripsub" : path +="/trip"; break;
 		case "golfsub" : path += "/golf"; break;
+		case "reservSub" : path += "/guestroom"; break;
+		case "questionSub": path += "/adminQnA";break;
+		case "infosub" : path += "/byeolsolInfo";break;
 		}
 		
 		return path;
